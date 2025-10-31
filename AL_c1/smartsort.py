55928c42a88b75dbec140d424f6c2896a29bcd74
Jici Li <lij167047@gmail.com>
2025-10-31 11:27:22 +0000

    Update smartsort.py

--- AL_c1/smartsort.py
+++ AL_c1/smartsort.py
@@ -26,16 +26,51 @@ sortedRunThreshold = 10
 
 def insertSort(A,m,n):
     """In-place Insert Sort on A[m],..,A[n-1]"""
-
+    while j >= m and not comp(A[j], key):
+          A[j+1] = A[j]
+          j -= 1
+    A[j+1] = key
 
 def merge(C,D,m,p,n):
     """Merge C[m],...,C[p-1] and C[p],...,C[n-1] into D[m],...,D[n-1]"""
-
+    if comp is None:
+        comp=lambda a,b:a<b 
+    i,j,k=m,p,m
+    while i<p and j<n:
+        if comp(C[i],C[j]):
+            D[k]=C[i]
+            i+=1
+        else:
+            D[k]=C[j]
+            j+=1
+        k+=1
+    while i<p:
+        D[k]=C[i]
+        i+=1
+        k+=1
+    while j<n:
+        D[k]=C[j]
+        j+=1
+        k+=1
 
 def greenMergeSort(A,B,m,n):
     """Merge Sort A[m],...,A[n-1] using just B[m],...,B[n-1] as workspace.
     Deferr to Insert Sort if length <= insertSortThreshold"""
+    if n - m <= insertSortThreshold:
+        insertSort(A,m,n)
+    else:
+        q1=m+(n-m)//4
+        q2=m+(n-m)//2
+        q3=m+3*(n-m)//4
 
+        greenMergeSort(A,B,m,q1)
+        greenMergeSort(A,B,q1,q2)
+        greenMergeSort(A,B,q2,q3)
+        greenMergeSort(A,B,q3,n)
+
+        merge(A,B,m,q1,q2)
+        merge(A,B,q2,q3,n)
+        merge(B,A,m,q2,n)
 
 # Provided code:
 
@@ -50,15 +85,87 @@ def greenMergeSortAll(A):
 def allSortedRuns(A):
     """Build and return queue of sorted runs of length >= sortedRunThreshold.
     Queue items should be pairs (i,j) such that A[i],...,A[j-1] is sorted."""
-
+    Q=PeekQueue()
+    n=len(A)
+    i=0
+    while i<n:
+        j=i+1
+        while j<n and comp(A[j-1],A[j]):
+            j+=1
+        if j-i>=sortedRunThreshold:
+            Q.add((i,j))
+        i=j
+    return Q
 
 def isWithinRun(Q,i,j):
     """Test whether A[i],...,A[j-1] is sorted according to info in Q."""
+    while not Q.isEmpty():
+        (a,b)=Q.peek()
+        if i<a:
+            return False
+        elif a<=i and j<=b:
+            return True
+        else:
+            Q.remove()
+    return False
+
+def insertSort(A,m,n):
+    for pos in range(m+1,n):
+        current=A[pos]
+        j=pos-1
+        while j>=m and A[j]>current:
+            A[j+1]=A[j]
+            j-=1
+        A[j+1]=current
+
+def merge(C,D,m,p,n):
+    i,j,k=m,p,m
+    
+    while i<p and j<n:
+        if C[i]<=C[j]:
+            D[k]=C[i]
+            i+=1
+        else:
+            D[k]=C[j]
+            j+=1
+        k+=1
+    
+    while i<p:
+        D[k]=C[i]
+        i+=1
+        k+=1
+
+    while j<n:
+        D[k]=C[j]
+        j+=1
+        k+=1
 
 
 def smartMergeSort(A,B,Q,m,n):
     """Improvement on greenMergeSort taking advantage of sorted runs."""
+    if isWithinRun(Q,m,n):
+        return
+ 
+    if n-m<=insertSortThreshold:
+        insertSort(A,m,n)
+        return
 
+    q1=m+(n-m)//4
+    q2=m+(n-m)//2
+    q3=m+3*(n-m)//4
+
+    if not isWithinRun(Q,m,q1):
+        smartMergeSort(A,B,Q,m,q1)
+    if not isWithinRun(Q,q1,q2):
+        smartMergeSort(A,B,Q,q1,q2)
+    if not isWithinRun(Q,q2,q3):
+        smartMergeSort(A,B,Q,q2,q3)
+    if not isWithinRun(Q,q3,n):
+        smartMergeSort(A,B,Q,q3,n)
+    
+    merge(A,B,m,q1,q2)
+    merge(A,B,q2,q3,n)
+    merge(B,A,m,q2,n)
 
 # Provided code:
 
